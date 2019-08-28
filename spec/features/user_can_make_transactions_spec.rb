52386990265613f92ea3e29e1787a3e8c3a685dd
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-08-28 19:17:47 +0100

    a client can deposit and withdraw money from their account, tests pass

--- spec/features/user_can_make_transactions_spec.rb
+++ spec/features/user_can_make_transactions_spec.rb
@@ -11,19 +11,3 @@ describe BankAccount do
     end
 
 end
-
-
-
-
-
-# require 'bank_account'
-
-# RSpec.describe BankAccount do
-#   subject(:account) { described_class.new }
-
-#   it 'can make transactions' do
-#     account.deposit(50)
-#     account.withdraw(20)
-#     expect(account.transactionsHistory.history.length).to eq(2)
-#   end
-# end
